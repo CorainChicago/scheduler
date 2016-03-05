fc17f25f69b94743168f22867bada0ea390dadff
CorainChicago <chaysmagan@gmail.com>
2016-03-04 23:25:10 -0600

    create seed file

--- scheduler/db/migrate/20160305021016_create_appointments.rb
+++ scheduler/db/migrate/20160305021016_create_appointments.rb
@@ -1,7 +1,6 @@
 class CreateAppointments < ActiveRecord::Migration
   def change
     create_table :appointments do |t|
-      t.string :day
       t.integer :tutor_id
       t.datetime :time
       t.integer :student_id
