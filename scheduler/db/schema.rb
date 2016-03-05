f2075846bc468bd6bdab310ae4982aa259ae49e8
CorainChicago <chaysmagan@gmail.com>
2016-03-04 21:06:16 -0600

    setup rspec and install factory_girl

--- scheduler/db/schema.rb
+++ scheduler/db/schema.rb
@@ -11,10 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160305020411) do
+ActiveRecord::Schema.define(version: 20160305021016) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "schedules", force: :cascade do |t|
+    t.string   "day"
+    t.integer  "student_id"
+    t.integer  "tutor_id"
+    t.datetime "time"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "first_name"
