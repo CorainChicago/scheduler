1ebe798a9708d00fdee282577615f1945eb4054f
CorainChicago <chaysmagan@gmail.com>
2016-03-05 21:42:41 -0600

    create basic show and create methods in controller

--- scheduler/app/controllers/users_controller.rb
+++ scheduler/app/controllers/users_controller.rb
@@ -4,6 +4,18 @@ class UsersController < ApplicationController
   end
 
   def show
+    @user = User.find(user_params)
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to root_url, :notice => "You signed up."
+    else
+      @errors = @user.errors.full_message
+      redirect root_path
+    end
   end
 
   def update
@@ -13,6 +25,10 @@ class UsersController < ApplicationController
   def delete
   end
 
-  
+  private
+
+  def user_params
+    params.require(:appointment).permit(:id, :tutor_id, :time, :student_id)
+  end
 
 end
